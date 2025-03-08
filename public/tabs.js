f56f87cf2680a7a9525e18cac4e03d56d65c3cb0
Julian <julian@alientechnology.com.au>
2025-03-08 23:19:21 +1100

    added some sample files

--- public/tabs.js
+++
@@ -1,9 +0,0 @@
-var objTabHandler = api.getTabHandler();
-var objTabWindow = null;
-
-if (objTabHandler.canCreateTab())
-{
-	objTabWindow = objTabHandler.createTab("childtab.php", "", "");
-	//objTabWindow.document.write('blah');
-	objTabWindow.document.title = 'blah';
-}

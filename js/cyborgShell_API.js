2bed93d3e0db21fda8ad3804d876b02eca41b615
Julian <julian@alientechnology.com.au>
2025-10-28 09:20:52 +1100

    Update cyborgShell_API.js and cyborgShell_API.min.js

--- js/cyborgShell_API.js
+++ js/cyborgShell_API.js
@@ -8,6 +8,103 @@ function getGUID()
 	});
 }
 
+function childTabHandler(objOptions_a)
+{
+	var m_objThis = this;
+	var m_objOptions = objOptions_a;
+	var m_strHandlerID = getGUID();
+
+	var m_objTab =
+	{
+		"id" : m_objOptions.id,
+		"caption" : 'Child (' + m_objOptions.id + ')',
+		"window" : m_objOptions.window,
+		"opener" : m_objOptions.window.opener
+	};
+
+	$(m_objOptions.window).bind('message', function (objEvent_a)
+	{
+		if ($.isFunction(m_objOptions.cbOnBroadcast))
+		{
+			var objJSON = null;
+			
+	//console.log('before:childTabHandler');
+			try
+			{
+				objJSON = JSON.parse(objEvent_a.originalEvent.data);
+			}
+			catch (err)
+			{
+				// do nothing
+			}
+	//console.log('after1:childTabHandler');
+
+			if (objJSON !== null)
+			{
+				if (objJSON.queue == undefined)
+				{
+					doNothing();
+				}
+				else
+				{
+					m_objOptions.cbOnBroadcast(objJSON.queue, objJSON.message, objJSON.messagedata);
+				}
+			}
+	//console.log('after2:childTabHandler');
+		}
+	}
+	);
+
+	// ====================================================================================
+	// PUBLICS ============================================================================
+
+	m_objThis.broadcast = function (strQueue_a, strMessage_a, objMessageData_a)
+	{
+		var objJSON =
+		{
+			"tabid" : m_objTab.id,
+			"originid" : m_objTab.id,
+			"origindescription" : m_objTab.caption,
+			"queue" : strQueue_a,
+			"message" : strMessage_a,
+			"messagedata" : objMessageData_a
+		};
+		//alert('child broadcast:' + JSON.stringify(objJSON));
+		m_objTab.opener.postMessage(JSON.stringify(objJSON), '*');
+	};
+
+	m_objThis.canCreateTab = function ()
+	{
+		return false;
+	};
+
+	m_objThis.getHandlerID = function ()
+	{
+		return m_strHandlerID;
+	};
+
+	m_objThis.getTabInfo = function ()
+	{
+		return m_objTab;
+	};
+
+	m_objThis.getTabs = function ()
+	{
+		return [
+			{
+				"id" : m_objTab.id,
+				"caption" : m_objTab.caption
+			}
+		];
+	};
+
+	m_objThis.ping = function ()
+	{
+		//logDebug('child ping: ' + m_objTab.id);
+		m_objThis.broadcast('system', 'ping', 'ping');
+	};
+}
+
 function parentTabHandler(objOptions_a)
 {
 	var m_objThis = this;
@@ -46,7 +143,11 @@ function parentTabHandler(objOptions_a)
 
 			if (objJSON !== null)
 			{
-				if (!(objJSON.queue == undefined))
+				if (objJSON.queue == undefined)
+				{
+					doNothing();
+				}
+				else
 				{
 					m_objOptions.cbOnBroadcast(objJSON.queue, objJSON.message, objJSON.messagedata);
 				}
@@ -208,7 +309,7 @@ function parentTabHandler(objOptions_a)
 		objTab.window = window.open(strURL_a + '?tabid=' + objTab.id + strForm + strParameters, objTab.caption);
 		return objTab.window;
 	};
-	
+
 	m_objThis.getHandlerID = function ()
 	{
 		return m_strHandlerID;
@@ -281,7 +382,7 @@ function cyborgShell_API(objGlobals_a)
 
 	// string functions
 	
-	this.getGuid = function()
+	this.getGUID = function()
 	{
 		return getGUID();
 	};
